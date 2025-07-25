507c9a1cc1ee77235c84d9dc1b115d33035abfc3
Maxime Moreillon <m.moreillon@gmail.com>
2025-07-25 11:56:59 +0900

    auth

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,8 @@
 import { recreateCronJobs } from "$lib/server/cron";
 import { init as mqttInit } from "$lib/server/mqtt";
+
+export { handle } from "./auth";
+
 export const init = async () => {
   mqttInit();
   recreateCronJobs();
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { SignIn, SignOut } from "@auth/sveltekit/components";
+  import { page } from "$app/state";
+</script>
+
+<a href="/alarms">Return</a>
+
+<h2 class="text-2xl my-4">Auth</h2>
+<div>
+  {#if page.data.session}
+    <div class="my-4">
+      <small>Signed in as</small>
+      <strong>{page.data.session.user?.name ?? "User"}</strong>
+    </div>
+    <SignOut>
+      <div slot="submitButton" class="btn">Sign out</div>
+    </SignOut>
+  {:else}
+    <div class="my-4">You are not signed in</div>
+    <SignIn>
+      <div slot="submitButton" class="btn">Sign in</div>
+    </SignIn>
+  {/if}
+</div>
